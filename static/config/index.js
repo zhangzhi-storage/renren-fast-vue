30ad2d95c88f6417df37e68c75f81a6f84614e13
zhangsan19798 <1979815592@qq.com>
2021-08-12 21:04:27 +0800

    配置文件

--- static/config/index.js
+++ static/config/index.js
@@ -3,9 +3,9 @@
  */
 ;(function () {
   window.SITE_CONFIG = {};
-
+  //8080网关端口  renren-fast用户服务名称
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:88/api';
+  window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/renren-fast';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
